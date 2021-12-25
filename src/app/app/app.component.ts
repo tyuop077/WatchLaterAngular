3766dab9f0c35319b2b6ea6979011200b1ed9cf0
tyuop077 <30123136+tyuop077@users.noreply.github.com>
2021-12-25 18:41:12 +0300

    Folder refactoring

--- src/app/app/app.component.ts
+++ src/app/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import {FormBuilder, FormGroup} from "@angular/forms";
-import {MovieService} from "../services/movie.service";
+import {MovieService} from "../../services/movie.service";
 
 @Component({
   selector: 'app-root',
--- src/app/app/app.module.ts
+++ src/app/app/app.module.ts
@@ -4,8 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {HttpClientModule} from "@angular/common/http";
-import { HeaderComponent } from './header/header.component';
-import { HeaderButtonComponent } from './header-button/header-button.component';
+import { HeaderComponent } from '../components/header/header.component';
+import { HeaderButtonComponent } from '../components/header-button/header-button.component';
 
 @NgModule({
   declarations: [
