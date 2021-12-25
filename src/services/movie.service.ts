12614309fb09ad0c96518b23e67000eb9df1d5b1
tyuop077 <30123136+tyuop077@users.noreply.github.com>
2021-12-25 17:40:43 +0300

    Implemented http fetch

--- src/services/movie.service.ts
+++ src/services/movie.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import {BehaviorSubject} from "rxjs";
 import {Movie} from "../entities/Movie";
+import {HttpClient} from "@angular/common/http";
 
 @Injectable({
   providedIn: 'root'
@@ -9,70 +10,18 @@ import {Movie} from "../entities/Movie";
 export class MovieService {
   movies$: BehaviorSubject<Movie[]> = new BehaviorSubject<Movie[]>(movies);
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+    http.get("https://wl.api.tyuop.tk/movies").subscribe((data: any) => {
+      movies = data;
+      this.movies$.next(data)
+    })
+  }
 
   search(query: string) {
     if (query.length < 1) return this.movies$.next([]);
-    const res = movies.filter(movie => movie.title.toLowerCase().startsWith(query.toLowerCase()));
+    const res = movies.filter(movie => movie.title.toLowerCase().includes(query.toLowerCase()));
     this.movies$.next(res)
   }
 }
 
-const movies: Movie[] = [
-  {
-    id: 1,
-    title: "Минари",
-    thumbnail_url: "https://wl.api.tyuop.tk/img/1_Minari.png",
-    rating: 6.9,
-    date: 2020
-  },
-  {
-    id: 2,
-    title: "Солнцестояние",
-    thumbnail_url: "https://wl.api.tyuop.tk/img/2_Solncestoaniye.png",
-    rating: 6.6,
-    date: 2019
-  },
-  {
-    id: 3,
-    title: "Манк",
-    thumbnail_url: "https://wl.api.tyuop.tk/img/3_Mank.png",
-    rating: 7.1,
-    date: 2020
-  },
-  {
-    id: 4,
-    title: "Девушка, подающая надежды",
-    thumbnail_url: "https://wl.api.tyuop.tk/img/4_Devushka.png",
-    rating: 6.8,
-    date: 2020
-  },
-  {
-    id: 5,
-    title: "Земля кочевников",
-    thumbnail_url: "https://wl.api.tyuop.tk/img/5_Zemlya.png",
-    rating: 7.3,
-    date: 2020
-  },
-  {
-    id: 6,
-    title: "Отец",
-    thumbnail_url: "https://wl.api.tyuop.tk/img/6_Otets.png",
-    rating: 7.9,
-    date: 2020
-  },
-  {
-    id: 7,
-    title: "Ещё по одной",
-    thumbnail_url: "https://wl.api.tyuop.tk/img/7_Drunk.png",
-    rating: 7.9,
-    date: 2020
-  },
-  {
-    id: 8,
-    title: "Душа",
-    thumbnail_url: "https://wl.api.tyuop.tk/img/8_Soul.png",
-    rating: 8.3,
-    date: 2020
-  }
-]
+let movies: Movie[] = []
